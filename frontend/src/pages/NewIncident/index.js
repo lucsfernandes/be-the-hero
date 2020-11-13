08a62d03ddbd2bc790c15f3240e0575567a03a1c
Lucas Fernandes <lbandeira845@gmail.com>
2020-11-13 12:08:55 -0300

    correção da função "handleNew Incident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -15,7 +15,7 @@ export default function Incidents() {
 
     const ongId = localStorage('ongId');
 
-    async function handleNewIncident() {
+    async function handleNewIncident(e) {
         e.preventDefault();
 
         const data = {
